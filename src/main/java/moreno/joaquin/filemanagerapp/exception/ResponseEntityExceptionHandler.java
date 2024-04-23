7ad9de0e232146ee42b29ede13bd47ef438fbe96
Joaquin Moreno <joaquinmorenovfx@gmail.com>
2024-04-23 03:24:18 -0500

    fixed error 404 page

--- src/main/java/moreno/joaquin/filemanagerapp/exception/ResponseEntityExceptionHandler.java
+++ src/main/java/moreno/joaquin/filemanagerapp/exception/ResponseEntityExceptionHandler.java
@@ -16,41 +16,41 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
-public class ResponseEntityExceptionHandler extends org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler {
-
-    public ResponseEntity<Object> handleMethodArgumentNotValid
-            (MethodArgumentNotValidException ex,
-             HttpHeaders headers,
-             HttpStatusCode httpStatusCode,
-             WebRequest webRequest){
-
-        Map<String,Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", httpStatusCode.value());
-
-        List<String> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
-
-        body.put("errors", errors);
-        return  new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
-    }
-
-
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    public ResponseEntity<Object> handleFileNotFoundException(
-            StorageFileNotFoundException ex, WebRequest request) {
-
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        //body.put("status", httpStatusCode.value());
-        body.put("message", ex.getMessage());
-
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-    }
-
-
-}
+//@ControllerAdvice
+//public class ResponseEntityExceptionHandler extends org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler {
+//
+//    public ResponseEntity<Object> handleMethodArgumentNotValid
+//            (MethodArgumentNotValidException ex,
+//             HttpHeaders headers,
+//             HttpStatusCode httpStatusCode,
+//             WebRequest webRequest){
+//
+//        Map<String,Object> body = new LinkedHashMap<>();
+//        body.put("timestamp", LocalDateTime.now());
+//        body.put("status", httpStatusCode.value());
+//
+//        List<String> errors = ex.getBindingResult()
+//                .getFieldErrors()
+//                .stream()
+//                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+//                .collect(Collectors.toList());
+//
+//        body.put("errors", errors);
+//        return  new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+//    }
+//
+//
+//    @ExceptionHandler(StorageFileNotFoundException.class)
+//    public ResponseEntity<Object> handleFileNotFoundException(
+//            StorageFileNotFoundException ex, WebRequest request) {
+//
+//        Map<String, Object> body = new LinkedHashMap<>();
+//        body.put("timestamp", LocalDateTime.now());
+//        //body.put("status", httpStatusCode.value());
+//        body.put("message", ex.getMessage());
+//
+//        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+//    }
+//
+//
+//}
